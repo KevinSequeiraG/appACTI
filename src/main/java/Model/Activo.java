f6675ec6bd28aa4c505af207235032e08af79d18
unknown <oscargsc16u@gmail.com>
2022-04-16 18:23:24 -0600

    Mensaje de Usuario y Registro de activos ready

---
+++ src/main/java/Model/Activo.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Oscar
+ */
+public class Activo {
+    String ID;
+    String Descripcion;
+    float Valor;
+    String idSede;
+    String FechaRegistro;
+    String idUsuario;
+    int Cantidad;
+
+    public Activo() {
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public float getValor() {
+        return Valor;
+    }
+
+    public void setValor(float Valor) {
+        this.Valor = Valor;
+    }
+
+    public String getIdSede() {
+        return idSede;
+    }
+
+    public void setIdSede(String idSede) {
+        this.idSede = idSede;
+    }
+
+    public String getFechaRegistro() {
+        return FechaRegistro;
+    }
+
+    public void setFechaRegistro(String FechaRegistro) {
+        this.FechaRegistro = FechaRegistro;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getCantidad() {
+        return Cantidad;
+    }
+
+    public void setCantidad(int Cantidad) {
+        this.Cantidad = Cantidad;
+    }
+    
+    
+}
